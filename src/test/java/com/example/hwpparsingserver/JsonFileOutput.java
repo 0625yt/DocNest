7198d23f90ce42e82df3afddfa73ed78280799db
hyt <yt001228@naver.com>
2025-08-16 16:03:08 +0900

    MAC 기준 파싱 모듈 라이브러리화 하여 적용 및 테스트까지 완료

--- src/test/java/com/example/hwpparsingserver/JsonFileOutput.java
+++ src/test/java/com/example/hwpparsingserver/JsonFileOutput.java
@@ -1,28 +1,28 @@
 package com.example.hwpparsingserver;
 
 import com.parse.document.DataExtractContext;
+import com.parse.document.DocumentExtractorDocx;
 import com.parse.document.common.Const;
-import com.parse.document.DocumentExtractorHwp;
-import com.parse.document.FileDataExtractorHwp;
+import com.parse.document.FileDataExtractorDocx;
 
-public class JsonFileOutput extends FileDataExtractorHwp {
+public class JsonFileOutput extends FileDataExtractorDocx {
 
-	private DocumentExtractorHwp documentExtractorHwp = new DocumentExtractorHwp();
+	private final DocumentExtractorDocx documentExtractorDocx = new DocumentExtractorDocx();
 	private boolean toJson = true;
-	
+
 	public static void main(String[] args) {
 		DataExtractContext context = new DataExtractContext();
 		JsonFileOutput testInstance = new JsonFileOutput();
 
-		String fileName = "";
-		
+		String fileName = "4.약관_신한CEO정기보험Ⅱ(무배당,보증비용부과형)_일반심사형_20230703_v2.2.docx";
+
 		testInstance.makeValue(context, testInstance.toJson, fileName);
 	}
 
 	public void makeValue(DataExtractContext context,boolean toJson, String fileName) {
-		getHWPFile(context, Const.YACK_GUAN_NAME, fileName);
+		getDocxFiles(context, Const.YACK_GUAN_NAME, fileName);
 		try {
-			processDocument(context, Const.YACK_GUAN_NAME, documentExtractorHwp, toJson, fileName);
+			processDocument(context, Const.YACK_GUAN_NAME, documentExtractorDocx, toJson, fileName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
